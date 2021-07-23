5a8a3f0a2c4333eb1f9392059251513af0043090
justin_maai1 <justin9300454@gmail.com>
2021-07-23 13:57:20 +0100

    create 'transfer_learn' function and modify 'main'

--- MRLogP.py
+++ MRLogP.py
@@ -1,5 +1,6 @@
 import sys 
 import os
+import time
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold
@@ -28,6 +29,8 @@ class Dataset_maker(object):
         self.y_val = None
         self.cpd_name = None
         self.cpd_name_val = None
+        self.scaler = None
+        self.y_class =None
 
     def create_training_set(self, infile_training, val_split=0.1):
      with tf.device("cpu:0"):
@@ -47,11 +50,13 @@ class Dataset_maker(object):
         del dataset
 
         #Create validation set by sampling binned logP proportional to the logP bins in the training set
+        y_class = y
         for index, bin in enumerate(range (-5, 10, 1)):
             #print (f"{index}, {bin} <= bi < {bin+1}, {np.where((y_train >= bin) & (y_train < bin+1))}, {y_train[np.where((y_train >= bin) & (y_train < bin+1))]}, {len(y_train[np.where((y_train >= bin) & (y_train < bin+1))])}")
             X_train_sub = x[np.where((y >= bin) & (y < bin+1))]
             y_train_sub = y[np.where((y >= bin) & (y < bin+1))]
             cpd_id_sub = cpd_name[np.where((y >= bin) & (y < bin+1))]
+            y_class = np.where((y >= bin) & (y < bin+1), index, y_class)
             X_train_1, X_test_1, y_train_1, y_test_1, cpd_name_train_1, cpd_name_test_1 = train_test_split(X_train_sub, y_train_sub, cpd_id_sub, test_size=val_split, random_state=0)
             if index == 0:
                 X_train_p = X_train_1
@@ -75,7 +80,9 @@ class Dataset_maker(object):
         self.y_val = y_test_p
         self.cpd_name = cpd_name_train_p
         self.cpd_name_val = cpd_name_test_p
-        return self.X, self.X_val, self.y, self.y_val, self.cpd_name, self.cpd_name_val, scaler
+        self.scaler = scaler
+        self.y_class = y_class
+        return self.X, self.X_val, self.y, self.y_val, self.cpd_name, self.cpd_name_val, self.scaler, self.y_class
 
     def create_testset(self, infile_testing, sc):
      with tf.device("cpu:0"):
@@ -99,7 +106,7 @@ class Dataset_maker(object):
         return self.X, self.y
 
 class Model(object):
-    def __init__(self, droprate=0.1, mid_h_layers=1, mid_h_nodes=1264, learning_rate=0.001, batch_size=32, working_dir="/home/justin/cnnLogP/logP-2021/dnn_training_tl_training_against_dlSet_2021/proportionalTestset_repeats/model_deployment/dnnLogP-2021-model_20210716_model_github"):
+    def __init__(self, working_dir, droprate=0.1, mid_h_layers=1, mid_h_nodes=1264, learning_rate=0.001, batch_size=32):
         self.droprate = droprate
         self.h_layers = mid_h_layers
         self.h_nodes = mid_h_nodes
@@ -139,22 +146,54 @@ class Model(object):
 
     def cv(self, X_train, y_train, cv=10, epochs=1):
         fold=0
+        y_class = Dataset_maker().y_class
         kf = StratifiedKFold(n_splits=cv, shuffle=False)
         for train_index, test_index in kf.split(X_train, y_class):
             X_train_fold = X_train[train_index]
             y_train_fold = y_train[train_index]
-            X_test_fold = X_train[test_index]
-            y_test_fold = y_train[test_index]
-            cpd_name_train_fold = cpd_name_train[train_index]
-            cpd_name_test_fold = cpd_name_train[test_index]
-            train_y_class, test_y_class = y_class[train_index], y_class[test_index]
-            print (X_train_fold.shape, y_train_fold.shape, cpd_name_train_fold.shape, X_test_fold.shape, y_test_fold.shape, cpd_name_test_fold.shape)
-            print (cpd_name_train_fold)
+            X_val_fold = X_train[test_index]
+            y_val_fold = y_train[test_index]
             checkpointer = [ModelCheckpoint(os.path.join(self.work_dir, f"model_{fold}_bestValidation_fromCV.hdf5"), monitor='val_loss', verbose=0, save_best_only=True, mode='min')]
-            history = self.classifier.fit(X_train_fold, y_train_fold, epochs=epochs, validation_data=(X_test_fold, y_test_fold), batch_size=self.chunksize, callbacks=[checkpointer])
+            history = self.classifier.fit(X_train_fold, y_train_fold, epochs=epochs, validation_data=(X_val_fold, y_val_fold), batch_size=self.chunksize, callbacks=[checkpointer])
 
-    def transfer_learning(self):
-        pass
+    def transfer_learning(self, X_train, y_train, epoch_1, epoch_2, lr_tl):
+        #Create a new model by resuing all the layers but the output later from the base model  
+        model_2_on_1 = Sequential(self.classifier.layers[:-1])
+
+        #Freeze all the reused layers but the output later in the new model
+        for _layer in model_2_on_1.layers:
+            _layer.trainable = False
+        #model_2_on_1(training = False)
+        model_2_on_1.summary()
+
+        #Add a new output layer to the new model
+        model_2_on_1.add(Dense(units=1, kernel_initializer='normal', activation='linear', name='dense_2'))
+
+        #Compile the new model
+        opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+        model_2_on_1.compile(optimizer=opt, loss=[root_mean_squared_error], metrics=[root_mean_squared_error])
+        model_2_on_1.summary()
+        print ("\n-----------------------------------------------------------------------------------------------")
+        print (f"Epochs for training new output layer:{epoch_1}, No. unfrozen layers:{_unfr_layer}, Epochs for fine-tuning unfrozen layers:{epoch_2}")
+        time.sleep(5)
+
+        #Optimise the new output layer
+        model_2_on_1.fit(X_train, y_train, epochs=epoch_1)
+
+        #Unfreeze reused layers
+        for _layer in model_2_on_1.layers[-2:-3-(_unfr_layer*_unfr_layer):-1]: 
+            _layer.trainable = True
+        #model_2_on_1(training = True)
+
+        #Compile the new model
+        opt = Adam(lr=lr_tl, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+        model_2_on_1.compile(optimizer=opt, loss=[root_mean_squared_error], metrics=[root_mean_squared_error])
+        model_2_on_1.summary()
+
+        #Tweak the reused layers and save the model
+        model_2_on_1.fit(X_train, y_train, epochs=epoch_2)
+        model_2_on_1.save("model-tl-"+str(no_model)+".hdf5")
+
     def load_predictor(self, model_file):
      with tf.device("cpu:0"):
         self.classifier = load_model(model_file, custom_objects={'root_mean_squared_error': root_mean_squared_error})
@@ -175,67 +214,46 @@ def main(infile_training, infile_testing, working_dir, model_file, train=False, 
          val_split=0.1, droprate_list=[0.1], hidden_layer_list=[1], hidden_node_list=[1264], learnrate_list=[0.001], chunksize_list=[32], epoch_list=[1], 
          repeats=2, cv=None):
     data = Dataset_maker()
-    X_train, X_val, y_train, y_val, _, _, scaler = data.create_training_set(infile_training=infile_training, val_split=val_split)
-    print (X_train.shape, X_val.shape, y_train.shape, y_val.shape)
+    X_train, X_val, y_train, y_val, _, _, scaler, y_class = data.create_training_set(infile_training=infile_training, val_split=val_split)
+    print (X_train.shape, X_val.shape, y_train.shape, y_val.shape, y_class.shape)
     if train:
         if cv:
-            print ("CV")
-            pass
+            print (f"\nPerforming {cv} fold cross validation")
+            all_com_list = [(droprate_list[i], hidden_layer_list[i], hidden_node_list[i], learnrate_list[i], chunksize_list[i], epoch_list[i]) for i in len(droprate_list)]
+            all_com_dict = {index+1: ele for index, ele in enumerate(all_com_list)}
+            for index, para_list in all_com_dict.items():
+                model = Model(droprate=para_list[0], mid_h_layers=para_list[1], mid_h_nodes=para_list[2], learning_rate=para_list[3], batch_size=para_list[4], working_dir=working_dir)
+                classifier = model.build_model()
+                model.cv(X_train=X_train, y_train=y_train, cv=10, epochs=para_list[5])
         else:
-           all_com_list = [(droprate, mid_h_layer, mid_h_nodes, learning_rate, batch_size, epochs) for droprate in droprate_list for mid_h_layer in hidden_layer_list for mid_h_nodes in hidden_node_list for learning_rate in learnrate_list for batch_size in chunksize_list for epochs in epoch_list] 
-           all_com_dict = {index+1: ele for index, ele in enumerate(all_com_list)}
-           for index, para_list in all_com_dict.items():
+            print ("\nPerforming hyperparameter scan using holdout validation")
+            all_com_list = [(droprate, mid_h_layer, mid_h_nodes, learning_rate, batch_size, epochs) for droprate in droprate_list for mid_h_layer in hidden_layer_list for mid_h_nodes in hidden_node_list for learning_rate in learnrate_list for batch_size in chunksize_list for epochs in epoch_list] 
+            all_com_dict = {index+1: ele for index, ele in enumerate(all_com_list)}
+            for index, para_list in all_com_dict.items():
                 print (f"Model: {index}")
                 model = Model(droprate=para_list[0], mid_h_layers=para_list[1], mid_h_nodes=para_list[2], learning_rate=para_list[3], batch_size=para_list[4], working_dir=working_dir)
                 classifier = model.build_model()
                 model.training(X_train=X_train, y_train=y_train, X_val=X_val, y_val=y_val, repeats=repeats, epochs=para_list[5])
 
-
-    #if transfer_learn:
-    if predict_logP:
-        data = Dataset_maker()
-        X_test, y_test= data.create_testset(infile_testing=infile_testing, sc=scaler)
+    if transfer_learn:
+        print ("\nPerforming transfer learning - Loading the pre-trained model")
         model = Model(working_dir=working_dir)
-        classifier = model.load_predictor(model_file=model_file)
-        rmse_result = rmse(y_test, classifier.predict(X_test))
-        print (classifier.predict(X_test), classifier.predict(X_test).shape)
+        model.load_predictor(model_file=model_file)
+
+        print ("\nPerforming transfer learning - Starting fine-tune")
+        model.transfer_learning(X_train=X_train, y_train=y_train)
+
+    if predict_logP:
+        print ("\nPredicting on the test set")
+        X_test, y_test = data.create_testset(infile_testing=infile_testing, sc=scaler)
+        model = Model(working_dir=working_dir)
+        model.load_predictor(model_file=model_file)
+        rmse_result = rmse(y_test, model.classifier.predict(X_test))
+        print (model.classifier.predict(X_test), classifier.predict(X_test).shape)
         print (f"RMSE: {rmse_result}")
 
 if __name__ == "__main__":
     with tf.device("cpu:0"):
-        data = Dataset_maker()
-        X_train, X_val, y_train, y_val, _, _, sc = data.create_training_set("/home/justin/cnnLogP/logP-2021/dnn_training_tl_training_against_dlSet_2021/proportionalTestset_repeats/6/ds-descriptors-eMols201905-DL-500k-flat_FP4.csv")
-        print (X_train.shape, X_val.shape, y_train.shape, y_val.shape)
-
-        #hidden_layer_list = [2,3,4]
-        hidden_layer_list = [2]
-        #hidden_node_list = [316, 474, 632, 1264]
-        hidden_node_list = [316]
-        #chunksize_list = [32, 64, 128]
-        chunksize_list = [32]
-        #epochs_list = [1, 2, 3, 5, 10, 15, 20, 25, 30, 35]
-        epochs_list = [1]
-        #droprate_list = [0.1, 0.2, 0.4]
-        droprate_list = [0.1]
-        #learnrate_list = [0.001, 0.0001]
-        learnrate_list = [0.001]
-        model = Model()
-        classifier = model.build_model()
-        #print (classifier)
-        model.training(X_train, y_train, X_val, y_val, repeats=1, epochs=1)
-
-
-        #X_martel_dl, y_martel_dl = data.create_testset("/home/justin/cnnLogP/logP-2021/ds-descriptors-martel_FP4_qed_dl.csv", sc)
-        del  X_train, X_val, y_train, y_val
-        X_physprop_dl, y_physprop_dl = data.create_testset("/home/justin/cnnLogP/logP-2021/ds-descriptors-physprop_3d_allowed_atoms_FP4_qed_dl.csv", sc)
-        #print (X_martel_dl.shape, y_martel_dl, y_martel_dl.shape)
-        #print (X_physprop_dl.shape, y_physprop_dl.shape)
-
-        classifier = model.load_predictor("/home/justin/cnnLogP/logP-2021/dnn_training_tl_training_against_dlSet_2021/transfer_learning_correct_2021/Architecture_1/model_294-2/4/model-tl-11.hdf5")
-        rmse_result = rmse(y_physprop_dl, classifier.predict(X_physprop_dl))
-        print (rmse_result)
-        #r=root_mean_squared_error_np(y_physprop_dl, classifier.predict(X_physprop_dl))
-        #print (K.eval(r))
         main(infile_training="/home/justin/cnnLogP/logP-2021/dnn_training_tl_training_against_dlSet_2021/proportionalTestset_repeats/6/ds-descriptors-eMols201905-DL-500k-flat_FP4.csv", 
              infile_testing="/home/justin/cnnLogP/logP-2021/ds-descriptors-physprop_3d_allowed_atoms_FP4_qed_dl.csv",
              working_dir="/home/justin/cnnLogP/logP-2021/dnn_training_tl_training_against_dlSet_2021/proportionalTestset_repeats/model_deployment/dnnLogP-2021-model_20210716_model_github",
