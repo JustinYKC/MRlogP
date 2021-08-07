63674d4625993c948f1d618c969c72c63caa7060
justin_maai1 <justin9300454@gmail.com>
2021-08-07 06:49:34 +0100

    modify function 'rmse'

--- mrlogp/metrics.py
+++ mrlogp/metrics.py
@@ -7,7 +7,7 @@ def root_mean_squared_error(y_true, y_pred):
 def rmse(y_true, y_pred):
     res=0
     for i in range(len(y_true)):
-        res=res+((y_true[i]-y_pred[i])**2)
+        res+=((y_true[i]-y_pred[i])**2)
     res=res/len(y_true)
     res=res**0.5
     return res
