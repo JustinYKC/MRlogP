6585c113ba73755fdb7ac23563d8055d35b5158a
justin_maai1 <justin9300454@gmail.com>
2021-08-07 06:55:29 +0100

    make program more modularised

--- mrlogp/mrlogp_model.py
+++ mrlogp/mrlogp_model.py
@@ -1,3 +1,5 @@
+from numpy.core.fromnumeric import shape
+from sklearn.utils import validation
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 from tensorflow.compat.v1.keras.backend import set_session
@@ -12,7 +14,7 @@ from tensorflow.compat.v1.keras.layers import PReLU
 from tensorflow.compat.v1.keras.models import load_model
 import numpy as np
 from .metrics import *
-import time
+import time, os
 from pathlib import Path
 
 #XXX Check imports above are actually used
@@ -26,7 +28,7 @@ class Model(object):
     as well as loading an existing model for further use.
     
     """  
-    def __init__(self, working_dir, droprate=0.2, mid_h_layers=1, mid_h_nodes=1264, learning_rate=0.0001, batch_size=32, build_classifier=True):
+    def __init__(self, working_dir, droprate=0.2, mid_h_layers=1, mid_h_nodes=1264, learning_rate=0.0001, batch_size=32):
         self.droprate = droprate
         self.h_layers = mid_h_layers
         self.h_nodes = mid_h_nodes
@@ -82,16 +84,20 @@ class Model(object):
             Labels used for validation.
 
         epochs: (int, optional)
-            Numbers set for training epochs. Defaults to 0.1.
+            Numbers set for training epochs. Defaults to 30.
         """
-        with tf.device("cpu:0"): 
-            checkpointer = [ModelCheckpoint(Path(self.work_dir)/Path(f"model.hdf5"), monitor='val_loss', verbose=0, save_best_only=True, mode='min')]
-            history = self.classifier.fit(X_train, y_train, epochs=epochs, validation_data=(X_val, y_val), batch_size=self.chunksize, callbacks=[checkpointer])
+        if X_val.shape[0] == 0 & y_val.shape[0] == 0:
+            validation_data = None
+        else: validation_data = (X_val, y_val)
+        checkpointer = [ModelCheckpoint(os.path.join(self.work_dir, "model_bestValidation.hdf5"), monitor='val_loss', verbose=0, save_best_only=True, mode='min')]
+        history = self.classifier.fit(X_train, y_train, epochs=epochs, validation_data=validation_data, batch_size=self.chunksize, callbacks=[checkpointer])
+        self.classifier.save(os.path.join(self.work_dir, "model_endTraining.hdf5"))
+        return history
 
-    @staticmethod
-    def transfer_learning(X_train, y_train, epoch_1, epoch_2, lr_on_tweaking,_unfr_layer):
+    def transfer_learning(self, X_train, y_train, pre_train_model, epoch_1, epoch_2, lr_on_tweaking, unfr_layer, chunksize):
         #Create a new model by resuing all the layers but the output later from the base model  
-        model_2_on_1 = Sequential(self.classifier.layers[:-1])
+        model_1 = Model.load_predictor(Path(pre_train_model))
+        model_2_on_1 = Sequential(model_1.layers[:-1])
 
         #Freeze all the reused layers but the output later in the new model
         for _layer in model_2_on_1.layers:
@@ -107,14 +113,14 @@ class Model(object):
         model_2_on_1.compile(optimizer=opt, loss=[root_mean_squared_error], metrics=[root_mean_squared_error])
         model_2_on_1.summary()
         print ("\n-----------------------------------------------------------------------------------------------")
-        print (f"Epochs for training new output layer:{epoch_1}, No. unfrozen layers:{_unfr_layer}, Epochs for fine-tuning unfrozen layers:{epoch_2}")
+        print (f"Epochs for training new output layer:{epoch_1}, No. unfrozen layers:{unfr_layer}, Epochs for fine-tuning unfrozen layers:{epoch_2}")
         time.sleep(5) # Allow the user to read the terminal output
 
         #Optimise the new output layer
         model_2_on_1.fit(X_train, y_train, epochs=epoch_1)
 
         #Unfreeze reused layers
-        for _layer in model_2_on_1.layers[-2:(-3*_unfr_layer)-1:-1]: 
+        for _layer in model_2_on_1.layers[-2:(-3*unfr_layer)-1:-1]: 
             _layer.trainable = True
         #model_2_on_1(training = True)
 
@@ -124,13 +130,12 @@ class Model(object):
         model_2_on_1.summary()
 
         #Tweak the reused layers and save the model
-        model_2_on_1.fit(X_train, y_train, epochs=epoch_2)
-        model_2_on_1.save("model-tl.hdf5")
-
+        model_2_on_1.fit(X_train, y_train, epochs=epoch_2, batch_size=chunksize)
+        model_2_on_1.save(os.path.join(self.work_dir, "model-tl.hdf5"))
+        
     @staticmethod
     def load_predictor(model_file:Path):
         # XXX Check if string, if so, cast to path, check file exists, if not raise error.
-        with tf.device("cpu:0"):
-            model=Model(model_file.parent)
-            model.classifier = load_model(model_file, custom_objects={'root_mean_squared_error': root_mean_squared_error})
-            return model
+            #model=Model(model_file.parent)
+            classifier = load_model(model_file, custom_objects={'root_mean_squared_error': root_mean_squared_error})
+            return classifier
